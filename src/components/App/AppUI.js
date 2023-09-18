c3713fb0ad23492c304ca0ba3e04506a89157f91
Carlos Bastidas <carlos.bastidas2911@gmail.com>
2023-09-18 16:05:07 -0400

    se utilizo useContext

--- src/components/App/AppUI.js
+++ src/components/App/AppUI.js
@@ -5,56 +5,49 @@ import TodoItem from "../TodoItem";
 import { TodoSearch } from "../TodoSearch";
 import { CreateTodoButton } from "../CreateTodoButton";
 import TodosLoading from "../TodosLoading";
-import { TodoContext } from "../../context";
+import { TodoContext, TodoProvider } from "../../context";
 
 function AppUI() {
+  const {
+    loading,
+    error,
+    searchValue,
+    todos,
+    setSearchValue,
+    setNewListItems,
+    totalTodos,
+    searchTodo,
+    handleChange,
+    DeleteTodo,
+  } = React.createContext(TodoProvider);
   return (
-    <TodoContext.Consumer>
-      {({
-        loading,
-        error,
-        searchValue,
-        todos,
-        setSearchValue,
-        setNewListItems,
-        completedTodos,
-        totalTodos,
-        searchTodo,
-        handleChange,
-        DeleteTodo,
-      }) => (
-        <div className="containerPrincipal">
-          <div className="container containerOne--textStyle">
-            <TodoCounter completed={completedTodos} total={totalTodos} />
-            <CreateTodoButton />
-          </div>
-          <div className="container containerTwo--Styles">
-            <TodoSearch
-              searchValue={searchValue}
-              setSearchValue={setSearchValue}
-            />
-            <TodoList>
-              {loading && <TodosLoading />}
-              {error && <p>Hubo un error al cargar los Todo's</p>}
-              {!loading && totalTodos === 0 && <p>Crea tu primer Todo 📋</p>}
-              {searchTodo.map((todo) => {
-                return (
-                  <TodoItem
-                    text={todo.text}
-                    completed={todo.completed}
-                    key={todo.text}
-                    todos={todos}
-                    setTodo={setNewListItems}
-                    handleChange={handleChange}
-                    DeleteTodo={DeleteTodo}
-                  />
-                );
-              })}
-            </TodoList>
-          </div>
-        </div>
-      )}
-    </TodoContext.Consumer>
+    <div className="containerPrincipal">
+      <div className="container containerOne--textStyle">
+        <TodoCounter />
+        <CreateTodoButton />
+      </div>
+      <div className="container containerTwo--Styles">
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+        <TodoList>
+          {loading && <TodosLoading />}
+          {error && <p>Hubo un error al cargar los Todo's</p>}
+          {!loading && totalTodos === 0 && <p>Crea tu primer Todo 📋</p>}
+          {searchTodo.map((todo) => {
+            return (
+              <TodoItem
+                text={todo.text}
+                completed={todo.completed}
+                key={todo.text}
+                todos={todos}
+                setTodo={setNewListItems}
+                handleChange={handleChange}
+                DeleteTodo={DeleteTodo}
+              />
+            );
+          })}
+        </TodoList>
+      </div>
+    </div>
   );
 }
 
--- src/components/TodoCounter/index.js
+++ src/components/TodoCounter/index.js
@@ -1,14 +1,19 @@
 import React from "react";
-import'./styles.css'
+import "./styles.css";
+import { TodoProvider } from "../../context";
 
-function TodoCounter({completed, total}) {
+function TodoCounter() {
+  const { completedTodos, totalTodos } = React.useContext(TodoProvider);
   return (
-  <div>
-    <h1 className='containerOne__Tittle'>TODOS</h1>
-    <p className="containerOne__Parrafo">{`${(completed === total && total !==0 ) ? 'Felicidades terminaste todas tus tareas 🥳' : `Has completado ${completed} de ${total} TODOS`}`}</p>
-  </div>
+    <div>
+      <h1 className="containerOne__Tittle">TODOS</h1>
+      <p className="containerOne__Parrafo">{`${
+        completedTodos === totalTodos && totalTodos !== 0
+          ? "Felicidades terminaste todas tus tareas 🥳"
+          : `Has completado ${completedTodos} de ${totalTodos} TODOS`
+      }`}</p>
+    </div>
   );
 }
 
 export { TodoCounter };
-
